14cabd2e860cf90243af350c1e5b76101b1c1296
Oleg Zimnik <79708122+Zimolev@users.noreply.github.com>
2021-12-21 14:41:05 +0400

    Update ChatStarter.java

--- Lesson3/client/ChatStarter.java
+++ Lesson3/client/ChatStarter.java
@@ -1,14 +1,9 @@
 package com.company.client;
 
-
-
-
 import com.company.client.comunication.ClientCommunication;
 import com.company.client.gui.ChatFrame;
 import com.company.logs.LogsFiles;
-
 import java.util.function.Consumer;
-
 
 public class ChatStarter {
 
@@ -16,18 +11,16 @@ public class ChatStarter {
     private final ClientCommunication communication;
     private String nick = "";
     private final LogsFiles logsFiles;
+    private int size = 0;
 
     public ChatStarter() {
 
         communication = new ClientCommunication();
         logsFiles = new LogsFiles();
 
-
-
         Consumer <String> outboxMessageConsumer = outboundMessage -> {
             communication.sendMessage(outboundMessage);
         };
-
 
         frame = new ChatFrame(outboxMessageConsumer);
            new Thread(()-> {
@@ -35,16 +28,25 @@ public class ChatStarter {
                    String inboxMessage = communication.receiveMessage();
                    if (inboxMessage.startsWith("/authok")) {
                        nick = inboxMessage.split("\\s+")[1];
-                       logsFiles.setFile(inboxMessage, nick);
                        frame.getInboundMessageConsumer().accept("* Вы авторизованы как: " + nick + "\n");
-                       //socket.setSoTimeout(0);
-                       //break;
+                       frame.getInboundMessageConsumer().accept("Загрузка последних 100 записей: ");
+                       logsFiles.setFile("", nick);
+                       logsFiles.loadFile(logsFiles.getFile());
+                       if (logsFiles.getFilelogs().size() < 100){
+                           size = logsFiles.getFilelogs().size();
+                       }
+                       else {
+                           size = 99;
+                       }
+                       for (int i = 0; i < logsFiles.getFilelogs().size(); i++) {
+                           frame.getInboundMessageConsumer().accept(((String) logsFiles.getFilelogs().get(i)));
+                       }
+
                    }
                    else {
                    frame.getInboundMessageConsumer().accept(inboxMessage);
                        logsFiles.setFile(inboxMessage, nick);}
                }
            }).start();
-
     }
 }
